7697b33d26957a2739cf045586804faa0211c72c
DilyanSvetiev <dsvetiev@gmail.com>
2024-05-23 16:23:07 +0300

    fix: ContactsList

--- src/views/ContactsList.jsx
+++ src/views/ContactsList.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import SearchUser from "../components/SearchUser/SearchUser";
+import MyContactsList from "../components/MyContactsList/MyContactsList";
 
 
 export default function ContactsList() {
 
     const { userData } = useContext(AppContext);
     console.log(userData)
-    return <SearchUser />
+    return (
+        <>
+            <SearchUser />
+            <MyContactsList />
+        </>
+    )
 }
-
