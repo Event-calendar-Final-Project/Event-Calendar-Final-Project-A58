8be240c0f3881e4ecbd0f31ae1ac943526991073
DilyanSvetiev <dsvetiev@gmail.com>
2024-05-23 17:15:55 +0300

    fix: MyContactsList

--- src/components/MyContactsList/MyContactsList.jsx
+++ src/components/MyContactsList/MyContactsList.jsx
@@ -1,24 +1,27 @@
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "../../context/AppContext";
-import { getUserContactsList } from "../../services/users.service";
+import { fetchUserByHandle } from "../../services/users.service";
 
 
 export default function MyContactsList() {
 
     const[myContacts, setMyContacts] = useState([]);
     const { userData } = useContext(AppContext);
-    console.log(userData);
-    console.log(myContacts);
+
     useEffect(() => {
-        getUserContactsList(userData.handle).then(setMyContacts);
-    }), [];
+        const contacts = Object.keys(userData.contacts || {});
+        Promise.all(contacts.map((handle) => fetchUserByHandle(handle)))
+            .then(setMyContacts);
+    }, [userData.contacts]); 
+
+    console.dir(myContacts);
 
     return (
         <div>
             <h1>My Contacts</h1>
             <ul>
                 {myContacts.map((contact) => (
-                    <li key={contact.uid}>{contact.handle}</li>
+                    <li key={contact.uid}>{contact.handle} {contact.email}</li>
                 ))}
             </ul>
         </div>
