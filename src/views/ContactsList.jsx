6c3f9deb45a76464ae41da3b915cba6139f7ea8d
DilyanSvetiev <dsvetiev@gmail.com>
2024-05-25 17:46:05 +0300

    add: dropdown menu for contacts lists option

--- src/views/ContactsList.jsx
+++ src/views/ContactsList.jsx
@@ -1,17 +1,25 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import SearchUser from "../components/SearchUser/SearchUser";
 import MyContactsList from "../components/MyContactsList/MyContactsList";
 
-
 export default function ContactsList() {
+  const { userData } = useContext(AppContext);
+  const [selectedOption, setSelectedOption] = useState('myContacts'); // State to track the selected option in the dropdown menu
 
-    const { userData } = useContext(AppContext);
-    console.log(userData)
-    return (
-        <>
-            <SearchUser />
-            <MyContactsList />
-        </>
-    )
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value); // Set selectedOption to the selected value when the dropdown menu changes
+  };
+
+  return (
+    <>
+      <SearchUser />
+      <select onChange={handleSelectChange}>
+        <option value="myContacts">My Contacts</option>
+        <option value="contactsLists">Contacts Lists</option>
+      </select>
+      {selectedOption === 'myContacts' && <MyContactsList />}
+      {selectedOption === 'contactsLists' && <ContactsLists />}
+    </>
+  );
 }
