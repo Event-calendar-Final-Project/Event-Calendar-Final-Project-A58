c9af963496dccd5cc7fc519c392c09f59b27ca46
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-05-28 23:59:11 +0300

    navlink-layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,58 @@ export default function Header () {
         await logoutUser();
         setAppState({ user: null, userData: null})
     };
-
     return (
+        <div>
+            <header>
+                <div className="navbar bg-base-100">
+                    <div className="flex-1">
+                        <NavLink to="/" className="btn btn-ghost text-xl">Home</NavLink>
+                        <div className="join join-horizontal">
+  <input type="radio" name="theme-buttons" className="btn theme-controller join-item" aria-label="Default" value="default"/>
+  <input type="radio" name="theme-buttons" className="btn theme-controller join-item" aria-label="Retro" value="retro"/>
+  <input type="radio" name="theme-buttons" className="btn theme-controller join-item" aria-label="Luxury" value="luxury"/>
+  <input type="radio" name="theme-buttons" className="btn theme-controller join-item" aria-label="Valentine" value="valentine"/>
+  <input type="radio" name="theme-buttons" className="btn theme-controller join-item" aria-label="Aqua" value="aqua"/>
+</div>
+                    </div>
+                    <div className="flex-none gap-2">
+                        <NavLink to="/events" className="btn btn-ghost text-xl">Events</NavLink>
+                        <NavLink to="/create-event" className="btn btn-ghost text-xl">Create Event</NavLink>
+                        <NavLink to="/contacts" className="btn btn-ghost text-xl">Contacts</NavLink>
+<NavLink to="/my-calendar" className="btn btn-ghost text-xl">My Calendar</NavLink>
+                        {user && userData ? (
+                            <>
+                                <div className="dropdown dropdown-end">
+                                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                        <div className="w-10 rounded-full">
+                                            <img alt="Profile" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                        </div>
+                                    </div>
+                                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                                        <li>
+                                            <NavLink to="/my-profile" className="justify-between">
+                                                My profile
+                                            </NavLink>
+                                        </li>
+                                        <li onClick={logout}><a href="/">Logout</a></li>
+                                    </ul>
+                                </div>
+                                <NavLink to="/my-profile" className="btn btn-ghost text-xl">Welcome ,{userData.handle}!</NavLink>
+                            </>
+                        ) : (
+                            <>
+                                <NavLink to="/login" className="btn btn-ghost text-xl">Login</NavLink>
+                                <NavLink to="/register" className="btn btn-ghost text-xl">Register</NavLink>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+/*     return (
         <header>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/events">Events</NavLink>
@@ -34,3 +84,4 @@ export default function Header () {
         </header>
     )
 }
+ */
