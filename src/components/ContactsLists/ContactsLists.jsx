5443794c3a6069f7d4dc6c72b5a347756ac0a9e2
DilyanSvetiev <dsvetiev@gmail.com>
2024-05-31 01:17:54 +0300

    update: ContactsLists.jsx for rerendering and displaying new contact list

--- src/components/ContactsLists/ContactsLists.jsx
+++ src/components/ContactsLists/ContactsLists.jsx
@@ -2,6 +2,7 @@ import CreateContactLists from "../CreateContactsLists/CreateContactLists";
 import DisplayContactsLists from "../DisplayContactsLists/DisplayContactsLists";
 import { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../../context/AppContext';
+import { fetchContactLists } from '../../services/users.service';
 
 export default function ContactsLists() {
 
@@ -9,13 +10,17 @@ export default function ContactsLists() {
     const [contactLists, setContactLists] = useState([]);
 
     useEffect(() => {
-        setContactLists(Object.keys(userData.contactLists));
-      }, [userData.contactLists]);
+
+fetchContactLists(userData.handle).then(setContactLists);
+      }, [userData]);
+      console.dir(contactLists);
 
     return (
         <div>
             <h1>Contacts List</h1>
-            <CreateContactLists onNewListAdded={(newList) => setContactLists(prevLists => [...prevLists, newList])} />
+            <CreateContactLists onNewListAdded={(newList) => {
+  setContactLists(prevLists => [...prevLists, newList]);
+}} />
             <DisplayContactsLists contactLists={contactLists} />
         </div>
     )
