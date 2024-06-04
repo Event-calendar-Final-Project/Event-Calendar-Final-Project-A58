8b9ec52645e1ba9a23547be9ecea54d567bbd0a1
DilyanSvetiev <dsvetiev@gmail.com>
2024-06-04 16:28:36 +0300

    update: HoursColumn.jsx positioning

--- src/components/HoursColumn/HoursColumn.jsx
+++ src/components/HoursColumn/HoursColumn.jsx
@@ -1,23 +1,19 @@
 export default function HoursColumn()  {
 
-    const hours = Array.from({ length: 24 }, (_, i) => i);
+    const hours = Array.from({ length: 25 }, (_, i) => i);
     const styles = {
         li: {
             height: '40px',
             lineHeight: '40px',
             textAlign: 'center',
-        },
-        ul: {
-            marginTop: '-20px'
         }
     };
 
     return (
         <ul style={styles.ul}>
-            <li style={styles.li}></li>
             {hours.map(hour => (
                 <li key={hour} style={styles.li}>
-                    {`${hour}:00`}
+                    {`${hour.toString().padStart(2, '0')}:00`}
                 </li>
             ))}
         </ul>
