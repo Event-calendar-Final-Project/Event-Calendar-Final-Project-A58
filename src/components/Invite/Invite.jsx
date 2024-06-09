52cb5f7c4ae1f5553763ccbac42efa010f445b2a
DilyanSvetiev <dsvetiev@gmail.com>
2024-06-09 20:57:41 +0300

    update: Invite.jsx state

--- src/components/Invite/Invite.jsx
+++ src/components/Invite/Invite.jsx
@@ -6,38 +6,8 @@ import { fetchContactLists, getUserContactsList } from '../../services/users.ser
 import { inviteUser } from '../../services/event.service';
 
 export default function Invite({ initialEvent }) {
-  const { userData } = useContext(AppContext);
-  const [showInviteOptions, setShowInviteOptions] = useState(false);
-  const [contacts, setContacts] = useState([]);
-  const [contactLists, setContactLists] = useState([]);
-  const [inviteHandle, setInviteHandle] = useState('');
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const contactLists = await fetchContactLists(userData.handle);
-        setContactLists(Object.keys(contactLists));
-        const contacts = await getUserContactsList(userData.handle);
-        setContacts(Object.keys(contacts));
-        console.log(contacts);
-        console.log(contactLists);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-  
-    fetchData();
-  }, [userData.handle])
-  const handleInviteUser = async (userHandle) => {
-    try {
-      console.log(userHandle)
-      await inviteUser(initialEvent.id, userHandle); // Use initialEvent.id directly
-      // Update UI or state as needed after successful invitation
-      console.log(`User ${userHandle} invited successfully to event ${initialEvent.id}`);
-    } catch (error) {
-      console.error("Error inviting user:", error);
-    }
-  };
+  const [showInviteOptions, setShowInviteOptions] = useState(false);
 
   return (
     <div>
@@ -51,7 +21,8 @@ export default function Invite({ initialEvent }) {
       )}
       {showInviteOptions && (
         <div className="flex flex-col gap-2">
-            {contacts && <InviteUser initialEvent={initialEvent}/>}
+            {<InviteUser initialEvent={initialEvent}/>}
+            {<InviteContactList initialEvent={initialEvent}/>}
         </div>
       )}
     </div>
