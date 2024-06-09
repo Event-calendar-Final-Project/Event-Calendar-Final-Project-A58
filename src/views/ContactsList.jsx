00ef1ef821e8e2f9dda61491e782b66cd0978e7d
DilyanSvetiev <dsvetiev@gmail.com>
2024-06-10 02:44:30 +0300

    update: ContactsList.jsx  props update with rerender trigger function and lifting the contacts state

--- src/views/ContactsList.jsx
+++ src/views/ContactsList.jsx
@@ -1,13 +1,36 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 import SearchUser from "../components/SearchUser/SearchUser";
 import MyContactsList from "../components/MyContactsList/MyContactsList";
 import ContactsLists from "../components/ContactsLists/ContactsLists";
+import { fetchUserByHandle } from "../services/users.service";
+import { getUserContactsList } from "../services/users.service";
 
 
 export default function ContactsList() {
   const { userData } = useContext(AppContext);
-  const [selectedOption, setSelectedOption] = useState('myContacts'); 
+  const [selectedOption, setSelectedOption] = useState('myContacts');
+  const[myContacts, setMyContacts] = useState([]);
+  const [triggerRerender, setTriggerRerender] = useState(false);
+
+  const handleUserAdded = () => {
+    setTriggerRerender(prevState => !prevState);
+  }; 
+
+  useEffect(() => {
+    // Use the handle from userData to fetch the updated contacts list
+    getUserContactsList(userData.handle)
+      .then(contacts => {
+        if (contacts) {
+          // Assuming contacts is an object where keys are handles
+          const contactHandles = Object.keys(contacts);
+          return Promise.all(contactHandles.map(handle => fetchUserByHandle(handle)));
+        }
+        return [];
+      })
+      .then(setMyContacts)
+      .catch(error => console.error("Failed to fetch contacts:", error));
+  }, [userData.handle, triggerRerender]); // Depend on userData.handle and triggerRerender
 
   const handleSelectChange = (event) => {
     setSelectedOption(event.target.value); 
@@ -15,12 +38,12 @@ export default function ContactsList() {
 
   return (
     <>
-      <SearchUser />
+      <SearchUser onUserAdded={handleUserAdded} />
       <select onChange={handleSelectChange}>
         <option value="myContacts">My Contacts</option>
         <option value="contactsLists">Contacts Lists</option>
       </select>
-      {selectedOption === 'myContacts' && <MyContactsList />}
+      {selectedOption === 'myContacts' && <MyContactsList myContacts={myContacts}/>}
       {selectedOption === 'contactsLists' && <ContactsLists />}
     </>
   );
