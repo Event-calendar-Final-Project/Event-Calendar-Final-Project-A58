892c2c7119b303484ff776f99833b2fd861fd0fa
DilyanSvetiev <dsvetiev@gmail.com>
2024-06-10 02:41:27 +0300

    update: ContactLists  props update with rerender trigger function

--- src/components/ContactsLists/ContactsLists.jsx
+++ src/components/ContactsLists/ContactsLists.jsx
@@ -5,23 +5,22 @@ import { AppContext } from '../../context/AppContext';
 import { fetchContactLists } from '../../services/users.service';
 
 export default function ContactsLists() {
-
     const { userData } = useContext(AppContext);
     const [contactLists, setContactLists] = useState([]);
+    const [listUpdateTrigger, setListUpdateTrigger] = useState(false);
 
     useEffect(() => {
-
-fetchContactLists(userData.handle).then(setContactLists);
-      }, [userData]);
-      console.dir(contactLists);
+        fetchContactLists(userData.handle).then(setContactLists);
+    }, [userData, listUpdateTrigger]);
 
     return (
         <div>
             <h1>Contacts List</h1>
             <CreateContactLists onNewListAdded={(newList) => {
-  setContactLists(prevLists => [...prevLists, newList]);
-}} />
+                setContactLists(prevLists => [...prevLists, newList]);
+                setListUpdateTrigger(trigger => !trigger); 
+            }} />
             <DisplayContactsLists contactLists={contactLists} />
         </div>
-    )
-} 
+    );
+}
